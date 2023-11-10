024382fa5a5ebaaf4042425cf54e0a941d9c2929
Juan Hernandez <juan.hernandez@gse.com.co>
2023-11-10 06:15:49 -0500

    cambios realizados al area de los servicios

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,73 +1,89 @@
-document.addEventListener('DOMContentLoaded', () => {
-   
-    const geologicoCard = document.getElementById('geologico-card');
-    const geologicoForm = document.getElementById('geologico-form');
-    geologicoCard.addEventListener('click', (event) => {
+document.addEventListener("DOMContentLoaded", () => {
+  const geologicoCard = document.getElementById("geologico-card");
+  const geologicoForm = document.getElementById("geologico-form");
+  geologicoCard.addEventListener("click", (event) => {
+    if (!geologicoForm.contains(event.target)) {
       toggleDisplay(geologicoForm, event);
+    }
+  });
+
+  const runtCard = document.getElementById("runt-card");
+  const runtInfoContainer = document.getElementById("runt-info-container");
+  runtCard.addEventListener("click", (event) => {
+    toggleDisplay(runtInfoContainer, event);
+  });
+
+  document.querySelectorAll(".contract-info").forEach((contract) => {
+    const details = contract.querySelector(".contract-details");
+    const contractHeader = contract.querySelector(".contract-header");
+    const editableCells = details.querySelectorAll(
+      'td[contenteditable="true"]'
+    );
+
+    // Evento clic para abrir o cerrar la celda de contrato
+    contractHeader.addEventListener("click", (event) => {
+      toggleDisplay(details, event);
     });
-  
-    const runtCard = document.getElementById('runt-card');
-    const runtInfoContainer = document.getElementById('runt-info-container');
-    runtCard.addEventListener('click', (event) => {
-      toggleDisplay(runtInfoContainer, event);
+
+    // Evento clic dentro de la celda de contrato (por ejemplo, la tabla)
+    details.addEventListener("click", (event) => {
+      event.stopPropagation(); // Evita que se propague el clic dentro de la celda
     });
-  
-    document.querySelectorAll('.contract-info').forEach(contract => {
-      const details = contract.querySelector('.contract-details');
-      contract.addEventListener('click', (event) => {
-        toggleDisplay(details, event);
+
+    // Evento clic para las celdas editables de la tabla
+    editableCells.forEach((editableCell) => {
+      editableCell.addEventListener("click", (event) => {
+        event.stopPropagation(); // Evita que se propague el clic dentro de las celdas editables
       });
     });
-  
-    loadContractDetails();
   });
-  
-  function toggleDisplay(element, event) {
-    if (event) event.stopPropagation(); 
-  
-    if (element.style.display === 'none' || element.style.display === '') {
-      element.style.display = 'block';
-      element.style.maxHeight = element.scrollHeight + "px";
-    } else {
-      element.style.maxHeight = '0';
-      setTimeout(() => {
-        element.style.display = 'none';
-      }, 300); 
-    }
-  }
-  
-  function saveDetails(contractId) {
-    const editableCells = document.querySelectorAll(`#${contractId} .contract-details td[contenteditable="true"]`);
-    const padresValue = editableCells[0].innerText;
-    const otpValue = editableCells[1].innerText;
-  
-   
-    const contractData = {
-      padres: padresValue,
-      otp: otpValue
-    };
-  
-    
-    localStorage.setItem(contractId, JSON.stringify(contractData));
-  
-    
-    alert('Detalles guardados con éxito');
-  }
-  
-  function loadContractDetails() {
-    document.querySelectorAll('.contract-info').forEach(contract => {
-      const contractId = contract.id;
-      const storedData = localStorage.getItem(contractId);
-      if (storedData) {
-        const contractData = JSON.parse(storedData);
-        const editableCells = contract.querySelectorAll('td[contenteditable="true"]');
-        if (editableCells.length >= 2) {
-          editableCells[0].innerText = contractData.padres;
-          editableCells[1].innerText = contractData.otp;
-        }
-      }
+
+  loadContractDetails();
+});
+
+function toggleDisplay(element, event) {
+  if (event) event.stopPropagation();
+
+  if (element.style.display === "none" || element.style.display === "") {
+    element.style.display = "block";
+    element.style.maxHeight = element.scrollHeight + "px";
+  } else {
+    element.style.maxHeight = "0";
+    setTimeout(() => {
+      element.style.display = "none";
     });
   }
-  
-  document.addEventListener('DOMContentLoaded', loadContractDetails);
-  
+}
+
+function saveDetails(contractId) {
+  const editableCells = document.querySelectorAll(
+    `#${contractId} .contract-details td[contenteditable="true"]`
+  );
+  const padesValue = editableCells[0].innerText;
+  const otpValue = editableCells[1].innerText;
+
+  const contractData = {
+    pades: padesValue,
+    otp: otpValue,
+  };
+
+  localStorage.setItem(contractId, JSON.stringify(contractData));
+  alert("Detalles guardados con éxito");
+}
+
+function loadContractDetails() {
+  document.querySelectorAll(".contract-info").forEach((contract) => {
+    const contractId = contract.id;
+    const storedData = localStorage.getItem(contractId);
+    if (storedData) {
+      const contractData = JSON.parse(storedData);
+      const editableCells = contract.querySelectorAll(
+        'td[contenteditable="true"]'
+      );
+      if (editableCells.length >= 2) {
+        editableCells[0].innerText = contractData.pades;
+        editableCells[1].innerText = contractData.otp;
+      }
+    }
+  });
+}
